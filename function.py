01a02802bbdf07f2aa8c4475cb9a57dd7273d028
Ongeri123 <ongerinewton10@gmail.com>
2025-08-05 09:45:05 +0300

    worked on functions

---
+++ function.py
@@ -0,0 +1,8 @@
+# we use def to definea function
+def greet(name,country):
+    print(name)
+    print(country)
+    print('Hi', name,'Your favourite country is', country)
+    print(f'Hi {name} Your favourite country is {country}')
+
+greet('Newton', 'Kenya')
